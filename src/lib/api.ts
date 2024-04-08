4a21b6a120734d114346f406935be32dab58c5c2
Dana <mitsunokehl@gmail.com>
2024-04-08 21:15:55 +0300

    layout + dashboard

---
+++ src/lib/api.ts
@@ -0,0 +1,44 @@
+export const getSummaryCategories = () => {
+  return Promise.resolve([
+    {
+      categoryId: 1,
+      categoryTitle: 'Products',
+      count: 4,
+    },
+    {
+      categoryId: 2,
+      categoryTitle: 'Products',
+      count: 8,
+    },
+    {
+      categoryId: 3,
+      categoryTitle: 'Products',
+      count: 26,
+    },
+    {
+      categoryId: 4,
+      categoryTitle: 'Products',
+      count: 1,
+    },
+    {
+      categoryId: 5,
+      categoryTitle: 'Products',
+      count: 37,
+    },
+    {
+      categoryId: 6,
+      categoryTitle: 'Products',
+      count: 22,
+    },
+    {
+      categoryId: 7,
+      categoryTitle: 'Products',
+      count: 4,
+    },
+    {
+      categoryId: 8,
+      categoryTitle: 'Products',
+      count: 4,
+    },
+  ]);
+};
