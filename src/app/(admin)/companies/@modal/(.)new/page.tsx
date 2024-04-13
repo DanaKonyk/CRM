2c2d2a05c31cae2dc56282480057f1448192a352
Dana <mitsunokehl@gmail.com>
2024-04-13 16:34:14 +0300

    fix bugs

--- src/app/(admin)/companies/@modal/(.)new/page.tsx
+++ src/app/(admin)/companies/@modal/(.)new/page.tsx
@@ -2,11 +2,16 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import CompanyFormModal from '@/app/components/company-form-modal';
+import CompanyForm from '@/app/components/company-form';
+import Modal from '@/app/components/modal';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   const router = useRouter();
-  return <CompanyFormModal show={true} onClose={() => router.back()} />;
+  return (
+    <Modal show={true} onClose={() => router.back()}>
+      <CompanyForm onSubmit={console.log} />
+    </Modal>
+  );
 }
