25e4bdff481e33b9b33bf2c4d56492469b15ef84
Dana <mitsunokehl@gmail.com>
2024-04-13 16:23:49 +0300

    add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Button from './components/button';
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main>
-      <h1 className="text-xl">Home page</h1>
+    <main className="p-6 max-w-sm mx-auto mt-12 bg-white rounded-xl shadow-lg flex flex-col gap-10 items-center space-x-4">
+      <h1 className="flex-1 text-3xl font-semibold text-gray-900">CRM</h1>
+      <Button onClick={() => router.push('/dashboard')}>Go to dashboard</Button>
     </main>
   );
 }
