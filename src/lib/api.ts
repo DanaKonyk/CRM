e737a63602adc7e48b87e80b75ca809b55373f22
Dana <mitsunokehl@gmail.com>
2024-04-16 09:37:39 +0300

    final

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -64,7 +64,7 @@ const stringifyQueryParams = (params: Record<string, string>) =>
 
 const sendRequest = async <T>(url: string, init?: RequestInit) => {
   const res = await fetch(url, init);
-  console.log(res);
+
   
   if (!res.ok) {
     throw new Error(await res.text());
